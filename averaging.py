241e8f974cdc464b696012fdfb5cc4369c4c9356
Saiy <sayalideo137@gmail.com>
2019-09-09 03:29:51 +0530

    Normalization

--- averaging.py
+++ averaging.py
@@ -1,14 +1,21 @@
 import pandas as pd
-
+import numpy as np
 roo        = "./roo1.csv"
 
-df = pd.read_csv(roo,usecols = ['Logical quotient rating','coding skills rating','public speaking points'])
+#wines = np.genfromtxt("./roo1.csv", delimiter=";", skip_header=1)
+data1 = np.loadtxt("./roo1.csv", delimiter=',', skiprows=1)
+data  = data1[0:16129,:]
+print(data.shape)
+myu,sig = [],[]
+for i in range(9):
+    mean = np.mean(data[:,i])
+    myu.append(mean)
+    sigm = np.sum(np.square(data[:,i]))/data.shape[0]
+    sigm = sigm**0.5
+    sig.append(sigm)
+    data[:,i] = (data[:,i]-mean)/sigm
+    #print(data[:,i])
+                                            
+#print(myu,sig) 
 
-data = {
-    'Logical quotient rating': {4: 2235, 7: 2225, 1: 2193, 5: 2274, 3: 2217, 2: 2254, 9: 2227, 6: 2176, 8: 2190},
-    'coding skills rating': {4: 2223, 2: 2297, 1: 2139, 6: 2230, 8: 2241, 3: 2233, 5: 2205, 9: 2216, 7: 2207}, 
-    'public speaking points': {8: 2203, 3: 2162, 5: 2157, 1: 2234, 6: 2227, 4: 2257, 9: 2265, 7: 2267, 2: 2219}, 
-}
-
-#(sum(click)+alpha)/(N+2*alpha)
-
+print(data[:,0])
